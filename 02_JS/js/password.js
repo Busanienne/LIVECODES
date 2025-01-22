fe0928889bce4fecdad529f0582dfdcdc52a24a2
busanienne <chezmigy@naver.com>
2025-01-22 22:02:57 +0900

    LIVECODES: 새로운 파일 추가 및 수정

---
+++ 02_JS/js/password.js
@@ -0,0 +1,7 @@
+let password = prompt("PASSWORD");
+        if (password == "korea123") {
+            window.open("https://www.busanienne.com");
+        }
+        else {
+            alert("암호가 다릅니다.");
+        }
